c23ff3e55ac743dc7ec9f6ebdb26fe95bfe892bd
sebast1an-bust4mante <sebastianbustamante@liceovvh.cl>
2026-04-08 14:22:31 -0400

    miercoles08

---
+++ 06_actividad_this_miercoles/script.js
@@ -0,0 +1,47 @@
+// Ejercicio 1 
+// function resaltar(elemento) {
+//     elemento.style.backgroundColor = "#dff0ff";
+// }
+
+// function normal(elemento) {
+//     elemento.style.backgroundColor = "white";
+// }
+
+// Ejercicio 2
+// function validarFormulario(formulario) {
+//     let correo = formulario.correo.value;
+
+//     if (correo === "") {
+//         document.getElementById("mensaje").textContent =
+//             "El correo no puede estar vacío";
+//         return false;
+//     }
+
+//     document.getElementById("mensaje").textContent =
+//         "Formulario enviado correctamente";
+//     return true;
+// }
+
+
+
+function click(this) {
+    this.style.borderColor = "#00fc2a";
+}
+
+function noClick(this) {
+    this.style.border = "2px solid #fc0000"
+}
+
+function validarForm(form) {
+    let name = form.nombre.value;
+    let edad = form.edad.value;
+    
+    if (name === "" || edad === "") {
+        document.getElementById("mensajeForm").textContent = "Las entradas no pueden estar vacías.";
+        return false;
+    } else {
+        document.getElementById("mensajeForm").textContent = "Registrado con éxito";
+        form.reset();
+        return true;
+    };
+};
